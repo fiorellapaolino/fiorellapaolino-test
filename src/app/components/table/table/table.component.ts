419b56db069621cd97c759ea4e61ba1156e27c6d
Fiorella Paolino <fiorellapaolino@gmail.com>
2022-04-20 02:01:05 +0200

    updated sort

--- src/app/components/table/table/table.component.ts
+++ src/app/components/table/table/table.component.ts
@@ -9,103 +9,47 @@ import { Users } from '../models/table';
 })
 export class TableComponent implements OnInit {
 
-/*   user: Users = {
-    userId: 0,
-    id: 0,
-    title: '',
-    completed: false
-  }; */
   users: Users[] = [];
-  isEven = false;
 
   constructor(
     private Table: TableService,
-    ) { }
+  ) { }
 
   ngOnInit(): void {
     this.createUsers();
-}
-
-createUsers() {
-  this.Table.createUser().subscribe(
-    (res) => {
-      this.users = res;
-      console.log(res)  
-    });
-}
-
-
-}
- /*  sortDataId() {
-    const sortedData = [...this.users].sort((a, b) => {
-      if (a.userId < b.userId) {
-        return -1   /* this.sortDataId(); */
-    /* this.isEven
-    this.isOdd 
-      }
-      if (a.userId > b.userId) {
-        return 1;
-      }
-      return 0;
-    });
-    sortedData;
-  } */
-
-
-
-
-/*   isEven(n: any) { //par
-    return n % 2 == 0;
- }
- 
-  isOdd(n: any) { //impar
-    return Math.abs(n % 2) == 1;
- } */
-
-/*   sortDataId() {
-    const sortedData = [...this.users].sort((a, b) => {
-      if (a.userId < b.userId) {
-        return -1;
-      }
-      if (a.userId > b.userId) {
-        return 1;
-      }
-      return 0;
-    });
-    sortedData;
-  } 
-
-/*   get even(): boolean {
-    const sortedData = [...this.users]
-    return this.users % 2 === 0;
   }
-  get odd(): boolean {
-    return !this.even;
-  } */
 
-/*   sortDataUserId() {
-    const sortedData = [...this.users].sort((a, b) => {
-      if (a.userId % 2 < b.userId) {
-        return 1;
-      }
-      if (a.userId % 2 > b.userId) {
-        return -1;
-      }
-      return 0;
-    });
-    sortedData;
+  createUsers() {
+    this.Table.createUser().subscribe(
+      (res) => {
+        this.users = res;
+        console.log(res)
+      });
   }
- */
-/*   sortDataId() {
+
+  sortEven() {
     const sortedData = [...this.users].sort((a, b) => {
-      if (a.userId < b.userId) {
-        return -1;
-      }
-      if (a.userId > b.userId) {
+      if (a.userId % 2 === 0) {
         return 1;
       }
-      return 0;
+      if (b.userId % 2 === 0) {
+        return -1;
+      }
+      return 0
     });
-    sortedData;
-  } */
+    this.users = sortedData;
+  }
 
+  sortOdd() {
+    const sortedData = [...this.users].sort((a, b) => {
+      if (a.id % 2 === 1) {
+        return 1;
+      }
+      if (b.id % 2 === 1) {
+        return -1;
+      }
+      return 0
+    });
+    this.users = sortedData;
+  }
+}
